b1bdb9ca38e97ec2e93ac891182ae8da5b53f0ce
Dominik Rámháp <ramhapd@sonrisa.hu>
2026-02-18 14:42:41 +0100

    Added basic endpoints

--- aspnet-todolist/Program.cs
+++ aspnet-todolist/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using aspnet_todolist.Models;
+
 namespace aspnet_todolist
 {
     public class Program
@@ -5,9 +8,33 @@ namespace aspnet_todolist
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello world!");
+
+            app.MapGet("/todolist", async (TodoDb db) => await db.Todos.ToListAsync());
+
+            app.MapPost("/todolist", async (Todo todo, TodoDb db) =>
+            {
+                db.Todos.Add(todo);
+                await db.SaveChangesAsync();
+
+                return Results.Created();
+            });
+
+            app.MapPut("/todolist/{id}/complete", async (int id, TodoDb db) =>
+            {
+                var todo = await db.Todos.FindAsync(id);
+
+                if (todo == null) return Results.NotFound();
+
+                todo.IsComplete = true;
+                await db.SaveChangesAsync();
+
+                return Results.Ok();
+            });
 
             app.Run();
         }
