68e4ce9d62f9de2916c3a6b55dcf351e7dfe4122
Dominik Rámháp <ramhapd@sonrisa.hu>
2026-02-18 14:21:40 +0100

    Created db

---
+++ aspnet-todolist/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using aspnet_todolist.Models;
+
+namespace aspnet_todolist
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
