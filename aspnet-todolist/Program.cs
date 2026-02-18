e9f75cd12f54744cdddb4afbb16c9c3e5b26ce4d
Dominik Rámháp <ramhapd@sonrisa.hu>
2026-02-18 13:56:58 +0100

    Initial commit

---
+++ aspnet-todolist/Program.cs
@@ -0,0 +1,14 @@
+
+namespace aspnet_todolist
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.Run();
+        }
+    }
+}
