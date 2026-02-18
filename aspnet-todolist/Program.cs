6d67443a0af46e56ba7de6143b6dae072333e534
Dominik Rámháp <ramhapd@sonrisa.hu>
2026-02-18 14:59:34 +0100

    Added GET endpoints

--- aspnet-todolist/Program.cs
+++ aspnet-todolist/Program.cs
@@ -14,14 +14,30 @@ namespace aspnet_todolist
 
             app.MapGet("/", () => "Hello world!");
 
-            app.MapGet("/todolist", async (TodoDb db) => await db.Todos.ToListAsync());
+            app.MapGet("/todolist", async (TodoDb db) =>
+            {
+                return await db.Todos.ToListAsync();
+            });
+
+            app.MapGet("/todolist/complete", async (TodoDb db) =>
+            {
+                return await db.Todos.Where(t => t.IsComplete).ToListAsync();
+            });
+
+            app.MapGet("/todolist/{id}", async (int id, TodoDb db) =>
+            {
+                var todo = await db.Todos.FindAsync(id);
+                if (todo == null) return Results.NotFound();
+
+                return Results.Ok(todo);
+            });
 
             app.MapPost("/todolist", async (Todo todo, TodoDb db) =>
             {
                 db.Todos.Add(todo);
                 await db.SaveChangesAsync();
 
-                return Results.Created();
+                return Results.Created($"/todolist/{todo.Id}", todo);
             });
 
             app.MapPut("/todolist/{id}/complete", async (int id, TodoDb db) =>
