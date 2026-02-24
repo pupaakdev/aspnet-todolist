b3295d23fa4ada4ab915821fd8d258323e4ecf57
Dominik Rámháp <ramhapdominik@gmail.com>
2026-02-24 17:13:14 +0100

    Added search

--- aspnet-todolist/Program.cs
+++ aspnet-todolist/Program.cs
@@ -43,7 +43,7 @@ namespace aspnet_todolist
             /// <param name="isComplete">Optional filter to get only completed or incomplete todos.</param>
             /// <returns>A list of todo items.</returns>
             /// <response code="200">Returns the list of todo items.</response>
-            app.MapGet("/api/todos", async (TodoDb db, bool? isComplete, bool? isDeleted, bool showDeleted = false, string? sortBy = null, string sortOrder = "asc") =>
+            app.MapGet("/api/todos", async (TodoDb db, bool? isComplete, bool? isDeleted, string ? search, string ? sortBy, bool showDeleted = false, string sortOrder = "asc") =>
             {
                 var query = db.Todos.AsQueryable();
 
@@ -56,6 +56,9 @@ namespace aspnet_todolist
                 if (isComplete.HasValue)
                     query = query.Where(t => t.IsComplete == isComplete.Value);
 
+                if (!string.IsNullOrEmpty(search))
+                    query = query.Where(t => t.Name!.Contains(search));
+
                 if (!string.IsNullOrEmpty(sortBy))
                 {
                     var isDescending = sortOrder.Equals("desc", StringComparison.OrdinalIgnoreCase);
@@ -64,7 +67,7 @@ namespace aspnet_todolist
                     {
                         "id" => isDescending ? query.OrderByDescending(t => t.Id) : query.OrderBy(t => t.Id),
                         "name" => isDescending ? query.OrderByDescending(t => t.Name) : query.OrderBy(t => t.Name),
-                        "iscomplete" => isDescending ? query.OrderByDescending(t => t.IsComplete) : query.OrderBy(t => t.IsComplete),
+                        "complete" => isDescending ? query.OrderByDescending(t => t.IsComplete) : query.OrderBy(t => t.IsComplete),
                         _ => query
                     };
                 }
@@ -73,7 +76,7 @@ namespace aspnet_todolist
             })
             .WithTags("Todos")
             .WithSummary("Retrieves all todo items")
-            .WithDescription("Gets a list of all todo items. Optionally filter by completion status using the isComplete query parameter. Sort using sortBy (id, name, iscomplete, isdeleted) and sortOrder (asc, desc) parameters.");
+            .WithDescription("Gets a list of all todo items. Optionally filter by completion status using the isComplete query parameter. Search todos by name using the search parameter. Sort using sortBy (id, name, iscomplete, isdeleted) and sortOrder (asc, desc) parameters.");
 
             /// <summary>
             /// Retrieves a specific todo item by id.
