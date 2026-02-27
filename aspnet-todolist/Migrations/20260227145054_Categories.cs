e6e28140faef04e4c9ca0db55399524bcf22596c
Dominik Rámháp <ramhapdominik@gmail.com>
2026-02-27 15:52:23 +0100

    Categories DB Migration

---
+++ aspnet-todolist/Migrations/20260227145054_Categories.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnet_todolist.Migrations
+{
+    /// <inheritdoc />
+    public partial class Categories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Todos",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Color = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_CategoryId",
+                table: "Todos",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Categories_CategoryId",
+                table: "Todos",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Categories_CategoryId",
+                table: "Todos");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Todos_CategoryId",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Todos");
+        }
+    }
+}
