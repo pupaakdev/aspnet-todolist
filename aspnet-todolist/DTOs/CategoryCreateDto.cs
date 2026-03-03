9a1dc65d5a804a58f22537a47e363d12d2be7c2f
Dominik Rámháp <ramhapdominik@gmail.com>
2026-03-03 16:07:38 +0100

    Added data annotations

--- aspnet-todolist/DTOs/CategoryCreateDto.cs
+++ aspnet-todolist/DTOs/CategoryCreateDto.cs
@@ -1,7 +1,14 @@
-﻿namespace aspnet_todolist.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspnet_todolist.DTOs
 {
     public record CategoryCreateDto(
+        [Required]
+        [MaxLength(50)]
         string Name,
+
+        [Required]
+        [RegularExpression(@"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$", ErrorMessage = "Color must be a valid HEX code")]
         string Color
     );
 }
--- aspnet-todolist/DTOs/TodoCreateDto.cs
+++ aspnet-todolist/DTOs/TodoCreateDto.cs
@@ -1,7 +1,12 @@
-﻿namespace aspnet_todolist.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspnet_todolist.DTOs
 {
     public record TodoCreateDto(
+        [Required]
+        [MaxLength(100)]
         string Name,
+
         int CategoryId
     );
 }
